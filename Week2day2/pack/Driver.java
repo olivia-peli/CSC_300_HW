f97c7b94fcf14d7613bb1b2e80233675dbf3344b
olivia-peli <129095466+olivia-peli@users.noreply.github.com>
2023-09-10 10:40:47 -0500

    hw 1

--- Week2day2/pack/Driver.java
+++ Week2day2/pack/Driver.java
@@ -1,5 +1,20 @@
-public class Driver{
-    public static void main(String[] args){
-        System.out.println("hello world");
+public class Driver {
+     public static void main(String[] args){
+    Salesman[] sales = new Salesman[8];
+    sales[0] = newSalesman("Blade", 0);
+    sales[1] = newSalesman("Kate", 1000);
+    sales[2] = newSalesman("John", 500);
+    sales[3] = newSalesman("Jon", 300);
+    sales[4] = newSalesman("Steven", 600);
+    sales[5] = newSalesman("Homer", 800);
+    sales[6] = newSalesman("Matt", 700);
+    sales[7] = newSalesman("Peter", 200);
+    Salesman.displayAllSaleman(sales);
+    System.out.println("Sorting...");
+    Salesman[] sorted = Salesman.SortList(sales);
+    Salesman.displayAllSaleman(sorted);
+    Salesman[] topFive = new Salesman.getTopFive(sorted);
+    System.out.println("Give me top 5 salesman list:");
+    Salesmann.displayAllSaleman(topFive);
     }
 }
--- Week2day2/pack/Salesman.java
+++ Week2day2/pack/Salesman.java
@@ -1,4 +1,4 @@
-public class Driver{
+public class Salesman{
 
     public String name;
     public float total;
@@ -16,8 +16,43 @@ public class Driver{
         return this .total;
     }
     
-    public setName(String name){
-        return this.name +": " +this.total;
+    public void setName(String name){
+        this.name = name;
+    }
 
+    public String display(){
+        return this.name + ": " + this.total;
+    }
+
+    public static Salesman[] SortList(Salesman[] list){
+        Salesman[] newList = new Salesman[list.length];
+        for (int i = 0; i < list.length; i++){
+            newList[i] = list[i];
+        }
+        for(int n = 0; n < list.length - 1; n++){
+            for(int m = 0; m < list.length - n - 1; m++){
+                if (newList[m + 1].total < newList[m].total){
+                    Salesman temp = null;
+                    temp = newList[m+1];
+                    newList[m + 1] = newList[m];
+                    newList[m] = temp;
+                }
+            }
+        }
+        return newList;
+    }
+    
+    public static void displayAllSaleman(Salesmann[] list){
+        for(int i = 0; i < list.length; i++){
+            System.out.println(list[i].display);
+        }
+    }
+
+    public Salesman[] getTopFive(Salesman[] sorted){
+        Salesman[] returnValue = new Salesman[5];
+        for(int i = sorted.length; i < 5; i++){
+            returnValue[i] = sorted[sorted.length - 1];
+        }
+        return returnValue;
     }
 }
